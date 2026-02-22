c49259e2b78df18619160fb88245454e07923ee3
Clauda <klodaalrakkad@gmail.com>
2026-02-22 10:02:59 +0300

    bill

--- resources/views/filament/resources/bill-resource/pages/add-bill-items.blade.php
+++ resources/views/filament/resources/bill-resource/pages/add-bill-items.blade.php
@@ -1,24 +1,4 @@
-{{-- <x-filament-panels::page>
-    <x-filament-panels::header :actions="$this->getHeaderActions()">
-        <x-slot name="heading">
-            إضافة مواد للمذكرة #{{ $record->bill_number }}
-        </x-slot>
-    </x-filament-panels::header>
-
-    {{ filament()->renderHook('panels::page.form.before') }}
-
-    <x-filament-panels::form wire:submit="save">
-        {{ $this->form }}
-
-        {{ filament()->renderHook('panels::page.form.actions.before') }}
-
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-            :full-width="$this->hasFullWidthFormActions()"
-        />
-    </x-filament-panels::form>
-</x-filament-panels::page> --}}
-
+ 
 
 
 <div>
